4c2da6673c0b084d9628982815355f18afbf01bf
Daudi Amani <dsaronin@gmail.com>
2011-02-11 19:02:06 -0800

    0.1.2 get banner gallery

--- lib/banner-rotator/additions.rb
+++ lib/banner-rotator/additions.rb
@@ -6,10 +6,12 @@ module ApplicationHelper
 # args:
 #   page -- Page obj for page we're on
 # mechanism is: use sub_page.title of 'banner.*' to indicate a presumed banner gallery
-# however, if 'banner global' exists, it is to be used for all pages
+# if no banner sub-page exists and if 'banner global' exists, it is to be used for all pages
 # ---------------------------------------------------------------------------  
   def get_banner_gallery( page )
-    return [] unless @page.present? && (banner_page = @page.parts.detect{|p| p.title =~ /^banner/i})
+    return [] unless @page.present? 
+    return [] unless (banner_page = @page.children.detect{|p| p.title =~ /^banner/i}) ||
+                     (banner_page = Page.first( :conditions => "title ILIKE '%banner global%'" ) )
     return banner_page.images
   end
 
