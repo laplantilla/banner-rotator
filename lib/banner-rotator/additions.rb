38f5a903fdb75956fb9411b9c1440acab52e1346
Daudi Amani <dsaronin@gmail.com>
2011-02-13 15:27:19 -0800

    db-dependent LIKE usage

--- lib/banner-rotator/additions.rb
+++ lib/banner-rotator/additions.rb
@@ -1,6 +1,16 @@
 
 module ApplicationHelper
   
+unless const_defined?('__SQL_LIKE')
+  case ActiveRecord::Base.configurations[RAILS_ENV]['adapter']
+    when 'postgresql'  then
+      __SQL_LIKE = "ILIKE"
+  else
+      __SQL_LIKE = "LIKE"
+  end  # case
+
+end
+  
 # ---------------------------------------------------------------------------  
 # get_banner_gallery -- returns list of page_images for the banner for a page
 # args:
@@ -11,7 +21,7 @@ module ApplicationHelper
   def get_banner_gallery( page )
     return [] unless page && page.present? 
     return [] unless (banner_page = page.children.detect{|p| p.title =~ /^banner/i}) ||
-                     (banner_page = Page.first( :conditions => "title ILIKE '%banner global%'" ) )
+                     (banner_page = Page.first( :conditions => "title ${__SQL_LIKE} '%banner global%'" ) )
     return banner_page.images
   end
 
